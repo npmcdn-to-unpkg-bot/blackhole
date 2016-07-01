42317cda10ac978be8aa87f56db4c5d472e61aaa
Markus Weltin <mweltinpdx@gmail.com>
2016-07-01 16:18:49 -0700

    fullintegration complies without error and includes gps now

--- fullIntegration/fullIntegration.ino
+++ fullIntegration/fullIntegration.ino
@@ -7,13 +7,11 @@
   BSD license, all text above must be included in any redistribution
  ***************************************************************************/
 
+#define DEBUG_OUTPUT 
+
 #include "Adafruit_Sensor.h"
 #include "Adafruit_BME280.h"
-
 #include "TinyGPS.h"
-TinyGPS gps;
-char szInfo[64];
-int sleep = 1000;
 
 #define BME_SCK D4
 #define BME_MISO D3
@@ -28,162 +26,79 @@ int sleep = 1000;
 #include "Adafruit_BNO055.h"
 #include "imumaths.h"
 
-// Adafruit_BME280 bme; // I2C
-// Adafruit_BME280 bme(BME_CS); // hardware SPI
 Adafruit_BME280 bme(BME_CS, BME_MOSI, BME_MISO,  BME_SCK);
-
-#define BNO055_SAMPLERATE_DELAY_MS (100)
-
 Adafruit_BNO055 bno = Adafruit_BNO055(55);
 
-char eventOut[64];
+TinyGPS gps;
+char dataStr[64];
+int sleep = 1000;
+float lat, lon;
+unsigned long age;
 
 void setup() {
-  Serial.begin(9600);
-  Serial1.begin(9600);
-
-  if (!bme.begin()) {
-    Serial.println("Could not find a valid BME280 sensor, check wiring!");
-  }
-
-  /* Initialise the sensor */
-  if(!bno.begin())
-  {
-    Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
-  }
-
-  delay(1000);
-
-  bno.setExtCrystalUse(true);
+    Serial.begin(9600);
+    Serial1.begin(9600);
+    if (!bme.begin()) 
+    {
+        Serial.println("Could not find a valid BME280 sensor, check wiring!");
+    }
+    /* Initialise the sensor */
+    if(!bno.begin())
+    {
+        Serial.print("Ooops, no BNO055 detected ... Check your wiring or I2C ADDR!");
+    }
+    delay(1000);
+    bno.setExtCrystalUse(true);
 }
-/**************************************************************************/
-/*
-    Arduino loop function, called once 'setup' is complete (your own code
-    should go here)
-*/
-/**************************************************************************/
+
+
 void loop(void)
 {
-  /* Get a new sensor event */
-  sensors_event_t event;
-  bno.getEvent(&event);
+    /* Get a new sensor event */
+    sensors_event_t event;
+    bno.getEvent(&event);
 
-  sprintf(eventOut, "{\"x\":\"%.7f\",\"y:\":%.7f\",\"z\":\"%.7f\"}",  event.orientation.x,  event.orientation.y,  event.orientation.z);
+    sprintf(dataStr, "{\"x\":\"%.7f\",\"y\":\"%.7f\",\"z\":\"%.7f\"}",  event.orientation.x,  event.orientation.y,  event.orientation.z);
 
-  Particle.publish("imu", eventOut);
+    #if defined DEBUG_OUTPUT
+        Serial.println( dataStr );
+    #else
+        Particle.publish("imu", dataStr);
+    #endif
 
-/* Wait the specified delay before requesting nex data */ 
-    delay(BNO055_SAMPLERATE_DELAY_MS);
+    sprintf(dataStr, "{\"temp\":\"%.7f\",\"pres\":\"%.7f\",\"alti\":\"%.7f\",\"humd\":\"%.7f\"}",  bme.readTemperature(), (bme.readPressure() / 100.0), bme.readAltitude(SEALEVELPRESSURE_HPA), bme.readHumidity() );
 
-    sprintf(eventOut, "{\"temp\":\"%.7f\",\"pressure:\":%.7f\",\"altitude\":\"%.7f\",\"humidity\":\"%.7f\"}",  bme.readTemperature(), (bme.readPressure() / 100.0), bme.readAltitude(SEALEVELPRESSURE_HPA), bme.readHumidity() );
-     Particle.publish("bme280", eventOut);
+    #if defined DEBUG_OUTPUT
+        Serial.println(dataStr);
+    #else
+        Particle.publish("imu", dataStr);
+    #endif
 
-//** gps loop **/
+    /** gps loop **/
     bool isValidGPS = false;
-    
+
     for (unsigned long start = millis(); millis() - start < 1000;){
         // Check GPS data is available
         while (Serial1.available()){
             char c = Serial1.read();
-            
             // parse GPS data
-            if (gps.encode(c))
+            if (gps.encode(c)){
                 isValidGPS = true;
+            }
         }
     }
 
     // If we have a valid GPS location then publish it
-    if (isValidGPS){
-        float lat, lon;
-        unsigned long age;
-    
+    if (isValidGPS)
+    {
         gps.f_get_position(&lat, &lon, &age);
-        
-        sprintf(szInfo, "%.6f,%.6f", (lat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : lat), (lon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : lon));
-	// Serial.println(szInfo);
-	Particle.publish("gps", szInfo);
+        sprintf(dataStr, "%.6f,%.6f", (lat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : lat), (lon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : lon));
+        #if defined DEBUG_OUTPUT
+            Serial.println(dataStr);
+        #else
+            Particle.publish("gps", dataStr);
+        #endif
     }
-    
-    delay(15000);
 
-   // delay(1000);
-}
-
-
-/**************************************************************************/
-/*
-    Displays some basic information on this sensor from the unified
-    sensor API sensor_t type (see Adafruit_Sensor for more information)
-*/
-/**************************************************************************/
-void displaySensorDetails(void)
-{
-  sensor_t sensor;
-  bno.getSensor(&sensor);
-  Serial.println("------------------------------------");
-  Serial.print  ("Sensor:       "); Serial.println(sensor.name);
-  Serial.print  ("Driver Ver:   "); Serial.println(sensor.version);
-  Serial.print  ("Unique ID:    "); Serial.println(sensor.sensor_id);
-  Serial.print  ("Max Value:    "); Serial.print(sensor.max_value); Serial.println(" xxx");
-  Serial.print  ("Min Value:    "); Serial.print(sensor.min_value); Serial.println(" xxx");
-  Serial.print  ("Resolution:   "); Serial.print(sensor.resolution); Serial.println(" xxx");
-  Serial.println("------------------------------------");
-  Serial.println("");
-  delay(500);
-}
-
-/**************************************************************************/
-/*
-    Display some basic info about the sensor status
-*/
-/**************************************************************************/
-void displaySensorStatus(void)
-{
-  /* Get the system status values (mostly for debugging purposes) */
-  uint8_t system_status, self_test_results, system_error;
-  system_status = self_test_results = system_error = 0;
-  bno.getSystemStatus(&system_status, &self_test_results, &system_error);
-
-  /* Display the results in the Serial Monitor */
-  Serial.println("");
-  Serial.print("System Status: 0x");
-  Serial.println(system_status, HEX);
-  Serial.print("Self Test:     0x");
-  Serial.println(self_test_results, HEX);
-  Serial.print("System Error:  0x");
-  Serial.println(system_error, HEX);
-  Serial.println("");
-  delay(500);
-}
-
-/**************************************************************************/
-/*
-    Display sensor calibration status
-*/
-/**************************************************************************/
-void displayCalStatus(void)
-{
-  /* Get the four calibration values (0..3) */
-  /* Any sensor data reporting 0 should be ignored, */
-  /* 3 means 'fully calibrated" */
-  uint8_t system, gyro, accel, mag;
-  system = gyro = accel = mag = 0;
-  bno.getCalibration(&system, &gyro, &accel, &mag);
-
-  /* The data should be ignored until the system calibration is > 0 */
-  Serial.print("\t");
-  if (!system)
-  {
-    Serial.print("! ");
-  }
-
-  /* Display the individual values */
-  Serial.print("Sys:");
-  Serial.print(system, DEC);
-  Serial.print(" G:");
-  Serial.print(gyro, DEC);
-  Serial.print(" A:");
-  Serial.print(accel, DEC);
-  Serial.print(" M:");
-  Serial.print(mag, DEC);
+    delay(sleep);
 }
